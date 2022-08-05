c62a0df3d5d19ab49347c361d15cffc60c42688d
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2022-08-05 17:21:36 +0200

    Removed 1p test endpoint, added prod endpoint and stored as a secret (#97)
    Removed 1p test endpoint, added prod endpoint and stored as a secret variable
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>

--- terraform/hetzner/main.tf
+++ terraform/hetzner/main.tf
@@ -4,29 +4,29 @@
 # }
 
 data "onepassword_item" "hcloud_token" {
-  vault  = "66qfxcmgwlhutunx6slav6fyve"
-  uuid   = "fog2zbfwo4r7ag5mgu5jftz7j4"
+  vault = "66qfxcmgwlhutunx6slav6fyve"
+  uuid  = "fog2zbfwo4r7ag5mgu5jftz7j4"
 }
 
 data "onepassword_item" "hcloud_hostname" {
-  vault  = "66qfxcmgwlhutunx6slav6fyve"
-  uuid   = "m4dsdf2ndph7m67czape24qscy"
+  vault = "66qfxcmgwlhutunx6slav6fyve"
+  uuid  = "m4dsdf2ndph7m67czape24qscy"
 }
 
 # Create a server
 resource "hcloud_server" "mail" {
 
-  name        = data.onepassword_item.hcloud_hostname.username
+  name = data.onepassword_item.hcloud_hostname.username
 
   server_type = "cx21"
   image       = "debian-10"
 
-  location    = "nbg1"
-  datacenter  = "nbg1-dc3"
+  location   = "nbg1"
+  datacenter = "nbg1-dc3"
 
-  backups     = true
+  backups = true
 
-  delete_protection = true
+  delete_protection  = true
   rebuild_protection = true
 
 }
