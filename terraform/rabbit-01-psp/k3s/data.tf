7d4b985ccbb5bd44f269c5b1fb7dcb6a053ddc1c
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2023-03-22 12:36:07 +0100

    Added k3s, nginx rtmp (#331)
    * Added k3s, nginx rtmp
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
    
    * Removed trailing space from images->file_name
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
    
    ---------
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>

---
+++ terraform/rabbit-01-psp/k3s/data.tf
@@ -0,0 +1,8 @@
+data "onepassword_item" "rabbit_01_psp_token" {
+  vault = "66qfxcmgwlhutunx6slav6fyve"
+  uuid  = "h7fhsftvpum7r4b3rnqznz4lym"
+}
+
+data "external" "rabbit_01_psp_token_otp" {
+  program = [ "../setup.sh" ]
+}
---
+++ terraform/rabbit-01-psp/k3s/provider.tf
@@ -0,0 +1,53 @@
+terraform {
+  cloud {
+    organization = "Fastnetserv"
+    workspaces {
+      name = "rabbit-01-psp-k8s"
+    }
+  }
+
+  required_version = ">= 1.3.7"
+
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.13.0"
+    }
+
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "1.1.4"
+    }
+
+  }
+}
+
+provider "proxmox" {
+  virtual_environment {
+    endpoint = data.onepassword_item.rabbit_01_psp_token.hostname
+    username = data.onepassword_item.rabbit_01_psp_token.username
+    password = data.onepassword_item.rabbit_01_psp_token.password
+    otp = data.external.rabbit_01_psp_token_otp.result.otp
+    insecure = true
+  }
+}
+
+provider "onepassword" {
+  # Speciry url or token
+  url   = var.onepassword_endpoint
+  token = var.onepassword_token
+
+  # The following env variable can be used as alternative of url and token
+  # OP_CONNECT_TOKEN
+  # OP_CONNECT_HOST
+  #token = data.external.login_in_one_password.result.token
+
+}
+
+variable "onepassword_token" {
+  sensitive = true
+}
+
+variable "onepassword_endpoint" {
+  sensitive = true
+}
