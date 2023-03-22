2c3f0d4284946d7b3c52dc7f48474bedcc107ccf
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2023-03-22 14:25:04 +0100

    Update setup.sh (#337)

--- terraform/rabbit-01-psp/setup.sh
+++ terraform/rabbit-01-psp/setup.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-echo '{"otp": "'$(curl -s "${{ secrets.OP_ENDPOINT }}"/v1/vaults/66qfxcmgwlhutunx6slav6fyve/items/h7fhsftvpum7r4b3rnqznz4lym -H "Authorization: Bearer "${{ secrets.OP_TOKEN }}"" | jq '.fields[]| select(.label=="token") | .totp' | tr -d '"')'"}'
+echo '{"otp": "'$(curl -s $OP_ENDPOINT/v1/vaults/66qfxcmgwlhutunx6slav6fyve/items/h7fhsftvpum7r4b3rnqznz4lym -H "Authorization: Bearer $OP_TOKEN" | jq '.fields[]| select(.label=="token") | .totp' | tr -d '"')'"}'
