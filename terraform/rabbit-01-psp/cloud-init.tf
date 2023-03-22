4cec445e51b9e6d68442260a4add96edaae1cb63
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2023-03-22 12:36:07 +0100

    Added k3s, nginx rtmp (#331)
    * Added k3s, nginx rtmp
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
    
    * Removed trailing space from images->file_name
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
    
    ---------
    
    Signed-off-by: Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>

--- terraform/rabbit-01-psp/cloud-init.tf
+++ terraform/rabbit-01-psp/cloud-init.tf
@@ -10,7 +10,7 @@ chpasswd:
   #list: |
   #  ubuntu:example
   expire: false
-hostname: web1.ddlns.net
+hostname: PLACEHOLDER.ddlns.net
 packages:
   - qemu-guest-agent
 users:
@@ -27,5 +27,37 @@ runcmd:
 EOF
 
     file_name = "ubuntu.cloud-config.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_file" "cloud_config_rtmp" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "rabbit-01-psp"
+
+  source_raw {
+    data = <<EOF
+#cloud-config
+chpasswd:
+  #list: |
+  #  ubuntu:example
+  expire: false
+hostname: PLACEHOLDER.ddlns.net
+packages:
+  - qemu-guest-agent
+users:
+  - default
+  - name: ubuntu
+    groups: sudo
+    shell: /bin/bash
+    ssh-authorized-keys:
+      - ${trimspace(tls_private_key.rtmp_key.public_key_openssh)}
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    lock_passwd: false
+runcmd:
+  - systemctl start qemu-guest-agent
+EOF
+
+    file_name = "ubuntu-rtmp.cloud-config.yaml"
   }
 }
