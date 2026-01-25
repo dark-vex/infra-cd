32d934574788d405593d46be26adb4ccc6e08c57
Daniele De Lorenzi <daniele.delorenzi@fastnetserv.net>
2026-01-25 19:06:26 +0100

    Update provider.tf

--- terraform/gozzi-01-bio/provider.tf
+++ terraform/gozzi-01-bio/provider.tf
@@ -32,8 +32,8 @@ provider "proxmox" {
 
 provider "onepassword" {
   # Speciry url or token
-  connect_url = var.onepassword_endpoint
-  token = var.onepassword_token
+  connect_url   = var.onepassword_endpoint
+  connect_token = var.onepassword_token
 
   # The following env variable can be used as alternative of url and token
   # OP_CONNECT_TOKEN
