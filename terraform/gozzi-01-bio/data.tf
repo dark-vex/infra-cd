3f94d5db774a6dad50ba372b6e4adb2bb962630b
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-04-04 11:20:16 +0200

    Remove OTP Token on gozzi01

--- terraform/gozzi-01-bio/data.tf
+++ terraform/gozzi-01-bio/data.tf
@@ -3,10 +3,6 @@ data "onepassword_item" "gozzi_01_bio_token" {
   uuid  = "mfukjucb7uuljtldpncj3erlz4"
 }
 
-data "external" "gozzi_01_bio_token" {
-  program = ["${path.module}/setup.sh"]
-}
-
 data "onepassword_item" "ssh_key" {
     uuid     = "fg6utxhi7yzuxmheo3a4dpqcaq"
     vault    = "66qfxcmgwlhutunx6slav6fyve"
