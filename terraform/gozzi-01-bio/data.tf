6e315346f6a5841fe6f2c60a392a98dd846d444d
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-04-04 12:45:37 +0200

    feat(proxmox,gozzi): Add API endpoint token and SSH (#1234)
    * feat(proxmox,gozzi): Add API endpoint token and SSH
    
    * Add external token and ssh keys
    
    * Remove OTP Token on gozzi01
    
    * Create setup.sh
    
    * Update data.tf
    
    * Update setup.sh
    
    * fix(terraform,gozzi): Fix setup.sh permission
    
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>

--- terraform/gozzi-01-bio/data.tf
+++ terraform/gozzi-01-bio/data.tf
@@ -2,3 +2,17 @@ data "onepassword_item" "gozzi_01_bio_token" {
   vault = "66qfxcmgwlhutunx6slav6fyve"
   uuid  = "mfukjucb7uuljtldpncj3erlz4"
 }
+
+data "external" "gozzi_01_bio_token" {
+  program = ["${path.module}/setup.sh"]
+}
+
+data "onepassword_item" "ssh_key" {
+    uuid     = "fg6utxhi7yzuxmheo3a4dpqcaq"
+    vault    = "66qfxcmgwlhutunx6slav6fyve"
+}
+
+data "onepassword_item" "ssh_key_new" {
+    uuid     = "duivcbljdj4nufcxr6wgwcnsrq"
+    vault    = "66qfxcmgwlhutunx6slav6fyve"
+}
