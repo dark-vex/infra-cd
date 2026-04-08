dc7c3949e8d26477cf1a80cbbe1e202ac1ac0be6
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-04-08 15:05:02 +0200

    Delete terraform/rabbit-01-psp directory

--- terraform/rabbit-01-psp/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo '{"otp": "'$(curl -s $OP_ENDPOINT/v1/vaults/66qfxcmgwlhutunx6slav6fyve/items/h7fhsftvpum7r4b3rnqznz4lym -H "Authorization: Bearer $OP_TOKEN" | jq '.fields[]| select(.label=="token") | .totp' | tr -d '"')'", "api_token": "'$(curl -s $OP_ENDPOINT/v1/vaults/66qfxcmgwlhutunx6slav6fyve/items/h7fhsftvpum7r4b3rnqznz4lym -H "Authorization: Bearer $OP_TOKEN" | jq '.fields[]| select(.label=="api_token") | .value' | tr -d '"')'"}'
