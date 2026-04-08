3e33c82d2c5cb4c68e8aaa2a7a1fa13bb0e65359
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-04-08 15:10:27 +0200

    feat(Terraform,rabbit): Remove legacy code from CI/CD and terraform folder (#1244)
    * feat(Terraform,rabbit): Remove legacy code from CI/CD and terraform folder
    
    * Delete terraform/rabbit-01-psp directory
    
    * feat(Terraform,gozzi-01-bio): Remove legacy Terraform code
    
    * feat(Terraform,gozzi-01-bio): Remove legacy CI/CD code

--- terraform/gozzi-01-bio/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = proxmox_virtual_environment_vm.docker
-  id = "gozzi-pve/501"
-}
--- terraform/gozzi-01-bio/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo '{"api_token": "'$(curl -s $OP_ENDPOINT/v1/vaults/66qfxcmgwlhutunx6slav6fyve/items/mfukjucb7uuljtldpncj3erlz4 -H "Authorization: Bearer $OP_TOKEN" | jq '.fields[]| select(.label=="api_token") | .value' | tr -d '"')'"}'
