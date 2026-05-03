cdbf1b38349e63169d3250b45222fe3e4296d0f8
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-05-03 10:00:37 +0200

    feat(grafana): Grafana Dashboards via Terraform (#1370)
    * feat(grafana): Grafana Dashboards via Terraform
    
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
    
    * feat(grafana): Grafana Dashboards via Terraform
    
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>

---
+++ terraform/grafana/folders.tf
@@ -0,0 +1,9 @@
+resource "grafana_folder" "kubenuc" {
+  title = "kubenuc"
+  uid   = "kubenuc"
+}
+
+resource "grafana_folder" "k8s_vms_daniele" {
+  title = "k8s-vms-daniele"
+  uid   = "k8s-vms-daniele"
+}
