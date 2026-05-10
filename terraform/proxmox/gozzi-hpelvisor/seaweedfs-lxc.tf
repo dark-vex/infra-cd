2716b9e48c399473720ed3f4e0735bf09f3737a2
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-05-10 11:56:37 +0200

    feat(terraform,ansible,seaweedfs): Added S3 replicated
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>

---
+++ terraform/proxmox/gozzi-hpelvisor/seaweedfs-lxc.tf
@@ -0,0 +1,44 @@
+module "hpelvisor_seaweedfs_lxc" {
+  source = "github.com/dark-vex/terraform-proxmox-lxc?ref=v1.0.0"
+  providers = {
+    proxmox = proxmox.hpelvisor
+  }
+
+  hostname    = "seaweedfs-hpelvisor"
+  vmid        = 704
+  node_name   = "hpelvisor"
+  description = "SeaweedFS storage node - replication 100"
+
+  cpu_cores      = 2
+  memory         = 4096
+  swap           = 0
+  disk_size      = 100
+  disk_datastore = "data-hdd"
+
+  template_file_id = proxmox_download_file.hpelvisor_ubuntu_24_04_lxc.id
+  os_type          = "ubuntu"
+
+  network_bridge         = "vmbr5"
+  network_interface_name = "eth0"
+  ip_config = {
+    ipv4_address = "dhcp"
+  }
+
+  console = {
+    enabled = true
+  }
+
+  ssh_keys = [
+    local.ssh_public_key,
+    local.ssh_public_key_new
+  ]
+  password     = data.onepassword_item.lxc_access.password
+  unprivileged = true
+
+  started       = true
+  start_on_boot = true
+
+  manage_user_account = false
+
+  tags = ["automation", "lxc", "seaweedfs", "storage", "replication-100"]
+}
