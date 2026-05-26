9a7f283dfe7633e684673ec20807cf9fb04e029d
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-05-26 23:25:14 +0200

    feat(oci): Defined VMs
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>

--- terraform/oci/k8s-armchair/outputs.tf
+++ terraform/oci/k8s-armchair/outputs.tf
@@ -1,15 +1,19 @@
 output "instance_id" {
   value = module.k8s_arm.id
+  sensitive = true
 }
 
 output "public_ip" {
   value = module.k8s_arm.public_ip
+  sensitive = true
 }
 
 output "private_ip" {
   value = module.k8s_arm.private_ip
+  sensitive = true
 }
 
 output "display_name" {
   value = module.k8s_arm.display_name
+  sensitive = true
 }
--- terraform/oci/teleport/outputs.tf
+++ terraform/oci/teleport/outputs.tf
@@ -1,15 +1,19 @@
 output "instance_id" {
   value = module.teleport.id
+  sensitive = true
 }
 
 output "public_ip" {
   value = module.teleport.public_ip
+  sensitive = true
 }
 
 output "private_ip" {
   value = module.teleport.private_ip
+  sensitive = true
 }
 
 output "display_name" {
   value = module.teleport.display_name
+  sensitive = true
 }
