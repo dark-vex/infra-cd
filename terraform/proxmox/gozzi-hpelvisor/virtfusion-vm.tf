548b626ac45d103be5a665f27d3003a11374446d
Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>
2026-09-24 09:27:44 +0200

    Update virtfusion-vm.tf
    Signed-off-by: Daniele De Lorenzi <2905124+dark-vex@users.noreply.github.com>

--- terraform/proxmox/gozzi-hpelvisor/virtfusion-vm.tf
+++ terraform/proxmox/gozzi-hpelvisor/virtfusion-vm.tf
@@ -1,11 +1,7 @@
-# CODE-29: dark-vex/terraform-proxmox-vm hardcodes prevent_destroy = true
-# unconditionally, so a plain block deletion would fail. destroy = false
-# tells Terraform to forget the resource without attempting to destroy it -
-# the actual VM is deleted separately, out of band.
 removed {
   from = module.hpelvisor_virtfusion_vm.proxmox_virtual_environment_vm.this
 
   lifecycle {
-    destroy = false
+    destroy = true
   }
 }
